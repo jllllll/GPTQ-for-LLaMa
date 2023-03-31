6d33c9132b66918f3fbf5bf199caaa3f33451381
nerodiafasciata <mark.e.appleby@gmail.com>
2023-03-31 01:07:37 -0500

    Revert "Update setup_cuda.py"
    This reverts commit 07dc7e3eee7d4f9b4936272ea2f0bf052b7c7c65.

--- setup_cuda.py
+++ setup_cuda.py
@@ -5,7 +5,7 @@ setup(
     name='quant_cuda',
     ext_modules=[
         cpp_extension.CUDAExtension(
-            'quant_cuda', ['quant_cuda.cpp', 'quant_cuda_kernel.cu'],
+            'quant_cuda', ['quant_hip.cpp', 'quant_hip_kernel.hip'],
         ),
         cpp_extension.CUDAExtension(
             'quant_cuda_old', ['quant_cuda_old.cpp', 'quant_cuda_old_kernel.cu'],
