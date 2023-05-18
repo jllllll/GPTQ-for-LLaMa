0236f5a6ea59d3484daeba181713eb85dfb6e07f
0cc4m <picard12@live.de>
2023-05-18 18:51:15 +0200

    Improve CUDA speed using O3 compile argument
    Co-authored-by: James Zhao <jyzhao.work@gmail.com>

--- setup.py
+++ setup.py
@@ -13,12 +13,15 @@ setup(
     ext_modules=[
         cpp_extension.CUDAExtension(
             "quant_cuda_v1", ["quant_cuda_v1/quant_cuda.cpp", "quant_cuda_v1/quant_cuda_kernel.cu"],
+            extra_compile_args={'nvcc': ['-O3']},
         ),
         cpp_extension.CUDAExtension(
             "quant_cuda_v2", ["quant_cuda_v2/quant_cuda.cpp", "quant_cuda_v2/quant_cuda_kernel.cu"],
+            extra_compile_args={'nvcc': ['-O3']},
         ),
         cpp_extension.CUDAExtension(
             "quant_cuda_v3", ["quant_cuda_v3/quant_cuda.cpp", "quant_cuda_v3/quant_cuda_kernel.cu"],
+            extra_compile_args={'nvcc': ['-O3']},
         ),
     ],
     cmdclass={"build_ext": cpp_extension.BuildExtension},
